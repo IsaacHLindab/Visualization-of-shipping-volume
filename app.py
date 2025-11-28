1e3fe59ac4cb318b5d40b484ff8b1814f68b1939
IsaacHLindab <isaac.hogfelt@lindab.com>
2025-11-28 09:28:43 +0100

    fix(app.py): allow iframe implementation in PowerBI

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from callbacks import register_callbacks
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
+# Allow iframe embedding and fix CORS to implement to Power BI HTML Content
+@app.server.after_request
+def add_iframe_headers(response):
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
 # Disable arrow key scrolling
 app.clientside_callback(
     """
@@ -23,8 +31,12 @@ app.clientside_callback(
     Input('keyboard-event-store', 'data')
 )
 
+# Set the layout
 app.layout = create_layout()
+
+# Register all callbacks
 register_callbacks(app)
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True, port=8050) # debug = True
